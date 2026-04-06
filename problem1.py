1eec4b7a4ec60130ca45ab83153536832369620b
andreilbarbosa <andreilimabarbosa@outlook.com>
2026-04-05 22:11:14 -0300

    feat: add other way of solve the problem 1.

--- problem1.py
+++ problem1.py
@@ -6,3 +6,17 @@ if number % 2 == 0:
     print(f"The number {number} is even!")
 else:
     print(f"The number {number} is odd!")
+
+#This is the first form, but also make using function:
+
+num = int(input("Let's start the test, disk a number:"))
+
+def verify():
+    if num % 2 == 0:
+        return True 
+
+
+if verify():
+    print(f"The number {num} is even!")
+else:
+    print(f"The number {num} is odd!")
